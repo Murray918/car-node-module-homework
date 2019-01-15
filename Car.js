a85629a728765bdc25d56603385f2354c702bf6f
ohheyitslance <ohheyitslance@ohheyitslance-2.local>
2019-01-15 09:12:06 -0600

    made those classes

---
+++ Car.js
@@ -0,0 +1,14 @@
+class car {
+    constructor(car) {
+        color = "hot rod red",
+        convertible = true,
+        speed = 0,
+        accelerate = function() {
+            speed++
+        },
+        decelerate = function() {
+            speed--
+        }
+     }
+}
+console.log(car)
